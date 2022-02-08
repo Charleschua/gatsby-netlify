374de279550197c2f349c2b8db333bf1d33d00fc
Charleschua <charleschua19@gmail.com>
2022-02-08 17:38:14 +0800

    Updated APIurl in config. Add package-lock.json to gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.DEPLOY_URL
+          ? "https://singapore-real-estate-launch.herokuapp.com"
+          : "http://localhost:1337",
         queryLimit: 1000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
